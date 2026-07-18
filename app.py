10c9a8bfa158c1b1077b7e5d7c6d55f85614858a
viditsmart <viditdsharma@gmail.com>
2026-07-18 10:13:24 -0500

    Extracted data from wttr.in

--- app.py
+++ app.py
@@ -20,3 +20,16 @@ fake_weather_data = {
 # Hint: To get the city, you would write: fake_weather_data["city"]
 st.write(fake_weather_data["main"]["temperature_f"])
 st.write(fake_weather_data["weather_conditions"][0]["severity"])
+
+# 1. Define the URL for Dallas weather in JSON format
+url = "https://wttr.in/Dallas?format=j1"
+
+# 2. Make the request to the internet
+response = requests.get(url)
+
+# 3. Convert the response into a Python dictionary
+weather_data = response.json()
+st.json(weather_data)
+
+current_temp = weather_data["current_condition"][0]["temp_F"]
+st.write(f"Current Temperature: {current_temp} degrees F")
